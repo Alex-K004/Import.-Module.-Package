783627125525e932459988f2d96c0a8c38d6b38e
Neto test <akochetkov1312@gmail.com>
2024-12-20 18:42:36 +0300

    initial commit

---
+++ application/main.py
@@ -0,0 +1,11 @@
+import datetime
+from application.people import get_employes
+from application.salary import calculate_salary
+
+dt = datetime.datetime.today()
+
+if __name__ == '__main__':
+    calculate_salary(2,2)
+    get_employes(3)
+    print(dt)
+
---
+++ application/people.py
@@ -0,0 +1,2 @@
+def get_employes(c):
+    print(c)
